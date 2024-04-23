9aa5c0db9b06458baee8a9193d6678371b7c4a1e
Michael Harris <mdillanh@gmail.com>
2024-04-22 21:15:40 -0500

    finished activity

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'show.html', {'product': product})
